295efca81474affe34d4781c69da3e61394940f1
yuya-konkon <yuuuuya08@icloud.com>
2020-03-31 10:29:31 +0900

    wip header&footer

--- index.php
+++ index.php
@@ -4,3 +4,60 @@ require_once('config.php');
 require_once('functions.php');
 
 connectDb();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KleidunG</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!-- ここはheader -->
+  <div class="flex-col-area">
+    <nav class="navbar navbar-expand-lg navbar-dark mb-5">
+      <a href="index.php" class="logo">KleidunG</a>
+      <div class="collapse navbar-collapse" id="navbarToggle">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <?php if ($_SESSION['id']) : ?>
+            <li class="nav-item">
+              <a href="sign_out.php">ログアウト</a>
+            </li>
+            <li class="nav-item">
+              <a href="new.php">New Post</a>
+            </li>
+          <?php else : ?>
+            <li class="nav-item">
+              <a href="sign_in.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a href="sign_up.php">New Account</a>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+
+  <!-- ここからフッター -->
+  <footer class="footer font-small">
+    <div class="footer-copyright text-center py-3 footer-font">
+      &copy;Copyright © 2020 KleidunG.All rights reserved
+    </div>
+  </footer>
+
+</body>
+
+</html>
