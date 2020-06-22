2d3416521fd2aedea9d29a574a5f4adae6fbf22a
yuya-konkon <yuuuuya08@icloud.com>
2020-06-22 15:41:43 +0900

    fix21

--- good_delete.php
+++ good_delete.php
@@ -1,6 +1,22 @@
 <?php
 
+require_once('config.php');
+require_once('functions.php');
+
 session_start();
-$no = $_REQUEST["no"];
-$ID =$_SESSION["USERID"];
-$sql = "DELETE FROM `favorite` WHERE `fav_user`='.$ID.' AND `id`='.$no.'";
+$id = $_GET['id'];
+
+$dbh = connectDb();
+$sql = 'SELECT * FROM favarites WHERE id = :id';
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+$favorete = $stmt->fetch();
+
+$sql_delete = 'DELETE FROM favorites WHERE id  = :id;';
+$stmt_delete = $dbh->prepare($sql_delete);
+$stmt_delete->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt_delete->execute();
+
+$url = $_SERVER['HTTP_REFERER'];
+header('Location: ' . $url);
